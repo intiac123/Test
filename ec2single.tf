7e794a2478e614a2bc3884e99252a9e2c471cc38
intiac123 <114056514+intiac123@users.noreply.github.com>
2022-10-13 14:03:12 +0530

    Update ec2single.tf

--- ec2single.tf
+++ ec2single.tf
@@ -18,7 +18,6 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  ingress_cidr_blocks = ["0.0.0.0/16"]
   tags = {
     Name = "ExampleAppServerInstance"
   }
